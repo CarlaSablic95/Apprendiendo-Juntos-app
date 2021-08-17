5704d0091f47631c1c323cbf520c923f78e8db90
CarlaSablic95 <carlasablic95@gmail.com>
2021-08-17 15:45:31 -0300

    Registro y login de usuario, creacion de la coleccion alumnos

--- js/my-app.js
+++ js/my-app.js
@@ -17,12 +17,23 @@ var app = new Framework7({
         // RUTAS GENERALES
       { path: '/', url: 'index.html', },
       { path: '/registro/', url: 'registro.html' },
-
+      { path: '/login/', url: 'login.html' },
+      { path: '/mi-perfil/', url: 'mi-perfil.html' },
+      { path: '/materias/', url: 'materias.html' },
 ]
  // ... other parameters
   });
 
-  var mainView = app.views.create('.view-main');
+  let mainView = app.views.create('.view-main');
+
+  // db hace la conexión a la BD
+ let db = firebase.firestore();
+
+ // creo la coleccion de usuarios
+ let colAlumnos = db.collection("alumnos");
+
+ // Email y pass del registro, las hago variables globales para poder usarlas en el login, y así compararlas
+ let emailReg = "", passReg = "", avatar = "", datosAlumno = "", nombre ="", emailLogin = "";
 
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
@@ -50,11 +61,12 @@ $$(document).on('page:init', '.page[data-name="index"]', function (e) {
 
         $$('#registro').on('click', function(){
           mainView.router.navigate('/registro/');
-          console.log("carga registro");
+          console.log("Carga registro");
         })
 
         $$('#login').on('click', function(){
           mainView.router.navigate('/login/');
+          console.info("Carga login");
         })
 })
 
@@ -71,24 +83,39 @@ $$(document).on('page:init', '.page[data-name="registro"]', function (e) {
   $$('img').on('click', function() { avatarUsuario(this.id) });
 });
 
-function registro() {
-  nombre = $$('#nombre').val();
-  email = $$('#emailRegistro').val();
-  passReg = $$('#passRegistro').val();
+function registro() { // 14:58hs
+  // las variables que guardan los datos del usuario y el objeto que contiene esos datos, debo ponerlos fuera de la función --> firebase.auth().createUserWithEmailAndPassword(emailReg, passReg)
+  let avatar = $$('.fotoPerfil').attr('src');
+  let nombre = $$('#nombre').val();
+  let emailReg = $$('#emailRegistro').val();
+  let passReg = $$('#passRegistro').val();
 
-  firebase.auth().createUserWithEmailAndPassword(email, passReg)
+ let datosAlumno = {
+    avatar,
+    nombre,
+    emailReg,
+    passReg
+  };
+
+
+
+  firebase.auth().createUserWithEmailAndPassword(emailReg, passReg)
   .then((userCredential) => {
     // Registrado
-    var user = userCredential.user;
-    // Avatar
-    
+    // var user = userCredential.user;
+    // Creaar la BD de alumnos y agregarle los datos
+    // GRACIAS AL SEÑOR 15:16HS, AGREGUÉ A LA BD LA COLECCIÓN DE USUARIOS Y SUS DATOS
+      colAlumnos.doc('alumno').set(datosAlumno);
     // nombre
-      console.info('Nombre: ' + nombre);
+      console.info('Nombre: ' + datosAlumno.nombre);
       //email
-      console.info('Email:' + user.email);
+      console.info('Email:' + datosAlumno.emailReg);
 
+      console.info('Avatar:' + datosAlumno.avatar);
       // pass
-      console.info('Pass: ' + passReg);
+      // console.info('Pass: ' + passReg);
+
+      mainView.router.navigate('/login/');
       
   })
   .catch((error) => {
@@ -96,7 +123,7 @@ function registro() {
     // var errorMessage = error.message;
 
     // Valido si el nombre que el usuario ingresa, es válido
-    if(!(/^[A-Za-zÑñÁáÉéÍíÓóÚú\s]+$/g.test(nombre))) return console.error(`${nombre} no es un nombre válido`);
+    if(!(/^[A-Za-zÑñÁáÉéÍíÓóÚú\s]+$/g.test(datosAlumno.nombre))) return console.error(`${datosAlumno.nombre} no es un nombre válido`);
 
     // Valido si la contraseña que el usuario ingresa, no exceda los 6 caracteres
     if(passReg > 6) return console.error("La contraseña no puede exceder los 6 caracteres");
@@ -104,8 +131,66 @@ function registro() {
   });
 }
 
+// MARTES 17/8/2021
+// De 11:28hs a 15:44hs
+/* QUÉ HICE: 
+- Registrar y loguear al usuario
+- Crear una colección de usuarios para la BD y agregarle datos con código, y no manualmente
+
+*/
+
+
 function avatarUsuario(id) {
   avatar = id;
   $$('.fotoPerfil').addClass("avatarElegido");
   console.info("Avatar elegido: ", avatar);
 }
+
+/*************************************************/
+
+// LOGIN
+$$(document).on('page:init', '.page[data-name="login"]', function (e) {
+  $$('#btnLogin').on('click', login);
+});
+
+function login() {
+
+  emailLogin = $$('#emailLogin').val();
+  passLogin = $$('#passLogin').val();
+
+  firebase.auth().signInWithEmailAndPassword(emailLogin, passLogin)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // Imprimir email de usuario y un alerta con un mensaje de éxito
+
+      // Una vez que se loguea, es llevado al inicio, materias
+      mainView.router.navigate('/materias/');
+  
+    console.info('Mi email: ' + emailLogin);
+    alert('Bienvenido/a');
+  })
+  .catch((error) => {
+    // var errorCode = error.code;
+    // var errorMessage = error.message;
+
+    if(emailLogin !== emailReg) return console.error('El email es incorrecto');
+
+    if(passLogin !== emailLogin) return console.error('La constraseña es incorrecta');
+  });
+}
+
+/*Mi Perfil de usuario */
+
+$$(document).on('page:init', '.page[data-name="perfil"]', function (e) {
+  console.info("Carga perfil del usuario");
+
+   // GRACIAS AL SEÑOR, RECIÉN 13:08HS PUDE MOSTRAR EN LOGIN, LA FOTO QUE EL USUARIO ELIJE
+   $$('#mi-avatar').attr('src', 'img/' + 'iconos/' + `${avatar}` + '.png');
+ 
+   $$('#btnRegistro').on('click', registro);
+
+   $$('#nomAlumno').html('Nombre: ' + `${datosAlumno.nombre}`);
+
+   $$('#emailAlumno').html('Email: ' + `${datosAlumno.emailReg}`);
+});
